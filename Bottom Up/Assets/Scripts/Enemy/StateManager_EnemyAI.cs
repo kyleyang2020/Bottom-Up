c1a55839d91ae5511bf675ce8196c5c28e2212a3
Kyle Yang <kyleyang2020@gmail.com>
2024-11-17 16:18:45 -0500

    ai state setup

---
+++ Bottom Up/Assets/Scripts/Enemy/StateManager_EnemyAI.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager_EnemyAI : MonoBehaviour
+{
+    [Header("Variables")]
+    public float detectionRadius;
+}
